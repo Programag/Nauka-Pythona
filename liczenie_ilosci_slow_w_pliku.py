79a33d7bde62bb74cbec9de9c9a32f9d00aaac03
Programag <prookrol@gmail.com>
2025-08-11 11:46:29 +0200

    Update

--- liczenie_ilosci_slow_w_pliku.py
+++ liczenie_ilosci_slow_w_pliku.py
@@ -1,11 +1,14 @@
 def count_words(word):
     with open("koty.txt", "r", encoding="UTF8") as file:
         words_counter = 0
+        file.read()
+        end = file.tell()
+        file.seek(0)
         while True:
             data = file.readline()
-            if word.capitalize() in data or word in data:
+            if word in data.lower():
                 words_counter += 1
-            else:
+            elif word not in data.lower() and file.tell() == end:
                 break
         return words_counter
         
